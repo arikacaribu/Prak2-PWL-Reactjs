973f4df94568fceac72c42328325fc08e1a58074
ari kacaribu <58227097+arikacaribu@users.noreply.github.com>
2022-05-31 23:04:12 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Pages/Home";
+import AOS from "aos";
+import { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "aos/dist/aos.css";
+import Kontak from "./Pages/Kontak";
+import Helmet from "react-helmet";
+
+function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+    });
+    AOS.refresh();
+  }, []);
+  return (
+    <Router>
+      <Helmet>
+        <title>Cristian Ari - Programmer</title>
+        <meta
+          name="description"
+          content="Saya Sangat termotivasi untuk terus belajar dan mengasah kemampuan dalam dunia pemrograman agar dapat tumbuh secara profesional. Saya yakin dengan kemampuan saya dapat menghasilkan ide yang menarik dalam membuat sebuah aplikasi"
+        />
+      </Helmet>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/kontak" exact component={Kontak} />
+        {/* <Route path="*" exact component={Error_404} /> */}
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
